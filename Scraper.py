f9915115233dc6b254fbdd671b0e50654880e06a
Marco <marco.valerioti01@gmail.com>
2024-03-12 15:26:51 +0100

    Revisione giovanni

--- Scraper.py
+++ Scraper.py
@@ -1,5 +1,4 @@
 import time
-#from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
 import requests
@@ -17,14 +16,13 @@ class Scraper:
     #Analizzare questo snippet
     #target.find_all(lambda tag:tag.name=="li" and re.search(regex,tag.text))
 
-    lower_bound = 10
+    lower_bound = 13
 
 
     regexs = [
-        re.compile(r'[F|f]inetuning'),
+        re.compile(r'[F|f]ine-tune[d]*'),
         re.compile(r'[I|i]talian'),
         re.compile(r'[E|e]nglish'),
-        re.compile(r'[L|l]ightweight]'),
         re.compile(r'[C|c](PU|pu)'),
         re.compile(r'[G|g](PU|pu)'),
         re.compile(r'[G|g](pt|PT)'),
@@ -32,133 +30,53 @@ class Scraper:
         re.compile(r'T5')
     ]
 
+    #rappresentazione Sum(CiXi)
+    #costo modello ideale z*
+    
 
     to_validate = {}
 
     def __init__(self, url_model:str) -> None:
-        #rappresentazione Sum(CiXi)
-        self.kws_wg = {
-        re.compile(r'[F|f]inetuning') : [False, 4],
-        re.compile(r'[I|i]talian') : [False,3],
-        re.compile(r'[E|e]nglish') : [False,3],
-        re.compile(r'[L|l]ightweight]') : [False, 1],
-        re.compile(r'[C|c](PU|pu)') : [False,2],
-        re.compile(r'[G|g](pt|PT)') : [False,1],
-        re.compile(r'(\d)*[BERT](\d)*') : [False,2],
-        re.compile(r'T5') : [False,3]
-        }
+            self.url = url_model
+            self.flag = True
+            self.kws_wg = {
+                self.regexs[0] : [False, 4],
+                self.regexs[1] : [False,3],
+                self.regexs[2] : [False,3],
+                self.regexs[3] : [False,2],
+                self.regexs[4] : [False,2],
+                self.regexs[5] : [False,3],
+                self.regexs[6] : [False,3],
+                self.regexs[7] : [False,3]
+            }
 
-        self.url = url_model
-        self.flag = True
-        #self.driver = webdriver.Firefox()
-        #self.driver.get(self.url)
-        #RICORDA SEMPRE DI PULIRE GLI URL DA SPAZI BIANCHI
-        try:
-            response = requests.get(self.url.strip())
-            if response.status_code == 200:
-                self.soup = BeautifulSoup(response.text,"html.parser")
-        except Exception as e:
-            #TO DO impostare una flag che blocca lo scrape
-            self.flag = False
-        
-        #self.soup = BeautifulSoup(self.driver.page_source,"html.parser")
-        #self.dizionario = {}
-
-
-
-
-    #def _extract_info_tags(self):
-        """
-        Funziona
-        """
-        
-        regex = re.compile("relative group flex items-center")
-
-        try:
-            info_tags = self.soup.find_all(class_=regex)
-
-            stripped = [tag.get_text(strip=True) for tag in info_tags]
-
-            result = self._lowerize_strings(stripped)
-
-            return result
-        
-        except Exception as e:
-            print(e)
-
-
-    #def _populate_dict(self):
-        """
-        Funziona
-        """
-
-        tags:list[str] = self._extract_info_tags()
-
-        for phrase in tags:
-            words = re.split(r',|\s',phrase)
-            for word in words:
-                tmp = word.strip()
-                if tmp != '':
-                    if tmp not in self.dizionario:
-                        self.dizionario[tmp] = 1
-                    else:
-                        self.dizionario[tmp] = self.dizionario[tmp]+1
-
-
-    #def _lowerize_strings(self, tokens:list[str]):
-        """
-        Funziona
-        """
-
-        result:list[str] = []
-
-        for token in tokens:
-            result.append(token.lower())
-        
-        return result
-    
-
-
-    #def scrape(self, keywords:list[str]):
-        """
-        Funziona
-        """
-
-        count = 0
-
-        not_found:list[str] = []
-
-        self._populate_dict()
-
-        lower_kws = self._lowerize_strings(keywords)
-
-        for kw in lower_kws:
+            #RICORDA SEMPRE DI PULIRE GLI URL DA SPAZI BIANCHI
             try:
-                if self.dizionario[kw] >= 1:
-                    count = count+1
-            except KeyError as e:
-                #TO-DO salvare quale tag non è presente per l'i-esimo modello.
-                not_found.append(kw)
+                response = requests.get(self.url.strip())
+                if response.status_code == 200:
+                    self.soup = BeautifulSoup(response.text,"html.parser")
+                else:
+                    self.flag = False
+            except Exception as e:
+                #TO DO impostare una flag che blocca lo scrape
+                self.flag = False
         
-        if count >= len(keywords)/ 3:
-            
-            self.validated[self.url] = [
-                #Molto costosa, sto creando una lista con i tag trovati
-                [x for x in lower_kws if x not in not_found],
-                not_found
-            ]
-        #self.driver.quit()
+
+
 
 
     def scrape(self):
         if(self.flag):
-            result = [ {regex : self.soup.find_all(string=regex)} for regex in self.regexs ]
-            for diz in result:
-                for key in diz:
-                    if len(diz[key] > 0):
-                        self.kws_wg[key][0] = True
+            # result = [ {regex : self.soup.find_all(string=regex)} for regex in self.regexs ]
+            # for diz in result:
+            #     for key in diz:
+            #         if len(diz[key]) > 0:
+            #             self.kws_wg[key][0] = True
+            for regex in self.regexs:
+                if self.soup.find(string=regex):
+                    self.kws_wg[regex][0] = True
             
-            if self._fun_obiettivo() > self.lower_bound:
+            if self._fun_obiettivo() >= self.lower_bound:
                 self.to_validate[self.url] = [key for key in self.kws_wg if self.kws_wg[key][0] == False]
 
     def _fun_obiettivo(self):
--- main.py
+++ main.py
@@ -1,51 +1,87 @@
 import time
-from Crawler import Crawler
 from Scraper import Scraper
+import threading
+import os
+
+
+# def task(urls):
+#     i = 1
+
+#     for url in urls:
+#         print(f"Il thread {threading.current_thread.__name__} sta elaborando il modello {i}", end='\r')
+#         i += 1
+
+#         scraper = Scraper(url)
+        
+#         scraper.scrape()
+
+
+# def split_into_chunks(urls, n_thread):
+#     avg_chunk_size = len(urls) // n_thread
+#     remainder = len(urls) % n_thread
+
+#     chunks = []
+#     index = 0
+
+#     for i in range(n_thread):
+#         chunk_size = avg_chunk_size + (1 if i < remainder else 0)
+#         chunk = urls[index : index + chunk_size]
+#         chunks.append(chunk)
+
+#         index += chunk_size
+    
+#     return chunks
+
+# def esecuzione_parallela(urls, n_thread):
+#     chunks = split_into_chunks(urls, n_thread)
+#     threads:list[threading.Thread] = []
+
+#     for chunk in chunks:
+#         thread = threading.Thread(target=task, args=(chunk,))
+#         thread.start()
+#         threads.append(thread)
+
+#     for thread in threads:
+#         thread.join()
 
 
 def main():
 
-    # crawler = Crawler("https://huggingface.co/models?pipeline_tag=text2text-generation&sort=trending")
-
-    # crawler.crawl()
 
     urls = []
 
-    # DESIDERED_KEYWORDS = [
-    #         "CPU",
-    #         "USE",
-    #         "USES",
-    #         "LANGUAGES",
-    #         "FINETUNING",
-    #         "TUNING",
-    #         "ITALIAN",
-    #         "ITA",
-    #         "IT",
-    #         "USE",
-    #         "TRAINING",
-    #     ]
 
-    with open("crawler_output3.txt","r") as file:
+    with open("crawler_output4.txt","r") as file:
         for riga in file:
             urls.append(riga)
 
 
+
+    i = 1
     start = time.time()
 
+    #esecuzione_parallela(urls, os.cpu_count())
+
     for url in urls:
+        print(f"Elaborando modello {i}", end ='\r')
+        i = i+1
+
         scraper = Scraper(url)
         
         scraper.scrape()
 
+
+    #scraper = Scraper("")
     result = scraper.get_scraped()
         
     end = time.time()
 
     print(f"Tempo impiegato: {end - start} s")
 
-    with open("scraper_output3.txt","w") as file:
+    with open("scraper_output10.txt","w") as file:
         for key in result:
-            file.write(f"Modello ottenuto: {key} \n Parametri non trovati: {', '.join(result[key])} \n")
+            file.write(f"Modello ottenuto: {str(key)} \nParametri non trovati: {', '.join(map(str,result[key]))} \n")
+            file.write("\n")
 
 if __name__ == "__main__":
     main()
